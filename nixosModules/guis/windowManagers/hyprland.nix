bccee21dd0bda272caf571bc8b9acf9d82f3fef8
Finekaled <infernokeeper1@gmail.com>
2026-02-15 19:27:17 +1100

    Created a module file for the Gnome, Niri and Hyprland GUIs, subdivided into WMs and DEs.

---
+++ nixosModules/guis/windowManagers/hyprland.nix
@@ -0,0 +1,6 @@
+{ config, pkgs, ... }:
+
+{
+  # Packages required to run the Hyprland Tiling Window Manager
+  programs.hyprland.enable = true;
+}
---
+++ nixosModules/guis/windowManagers/niri.nix
@@ -0,0 +1,6 @@
+{ config, pkgs, ... }:
+
+{
+  # Packages required to run the Niri Scrolling Window Manager.
+  programs.niri.enable = true;
+}
